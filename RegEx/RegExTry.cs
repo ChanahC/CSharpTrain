66d55fa82c72f9a91423109b89d0c00287d12dab
ChanahC <ceva1116@gmail.com>
2013-05-05 21:54:10 +0800

    Regex trial added

---
+++ RegEx/RegExTry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace QuickSharp
+{
+    public class Program{
+        private static void Main(){
+            string sample = "Chan, Chan,Chan, Chanx";
+            Regex x = new Regex(" |, "); // initializes a regex which detects space or comma and space
+            foreach (string sub in x.Split(sample)){ //uses a splitter function given the regex initialized to detect space or comman and space
+                Console.WriteLine("{0}", sub);
+            }
+        }
+    }
+}
