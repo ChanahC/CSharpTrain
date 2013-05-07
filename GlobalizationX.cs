8953dd6954f9666730d664b3dcc490908b3374f5
ChanahC <ceva1116@gmail.com>
2013-05-07 21:49:47 +0800

    Globalization and Threading

---
+++ GlobalizationX.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace QuickSharp{
+    public class GlobalizationX{
+        private static void Main()
+        {
+            Console.WriteLine(Thread.CurrentThread.CurrentCulture.Name);
+            //This gets the current thread's (which in this case is the main thread which executes Main())
+            // Culture or localization
+            CultureInfo x = new CultureInfo("ko-KR"); //creating a culture info class
+            Thread.CurrentThread.CurrentCulture = x; //setting the threads culture info into x
+            Console.WriteLine(Thread.CurrentThread.CurrentCulture.Name);
+            Console.WriteLine();
+            //This is for listing all the possible values for culture info
+             foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures)){
+              Console.WriteLine("{0} | {1}", ci.EnglishName, ci.Name);
+            }
+            CultureInfo currentUI = CultureInfo.CurrentUICulture; //gets the UI culture of the current running app
+            Console.WriteLine(currentUI);
+            InputLanguage inL = InputLanguage.CurrentInputLanguage;
+            //getting the current input language
+            CultureInfo xinfo = inL.Culture;
+            //getting the culture of the current input language
+            Console.WriteLine("Culture identifier = {0}\n"+"Display name = {1}\n"+"Calender = {2}\n",xinfo.ToString(),xinfo.DisplayName,xinfo.Calendar.ToString());
+          
+        }
+    }
+}
---
+++ ThreadTry1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+public class ThreadTry1{
+    public static void Main()
+    {
+        Thread thread = new Thread(WorkerOperation); 
+        /*Thread is in the namespace Threading... it requires a delegate function to be
+        * which should have a private static void attribute. The delegate function contains
+        * the activities that would be done by the thread. When the thread has a while or a loop that works
+        * for a long time, the thread would be doin the job on the delegate function.
+        * the delegate function could accept input since when you initialize the thread it just sets
+        * the start to call the delegate function.
+        */ 
+        thread.Start(); //This is to start a thread
+        //thread.Suspend(); //This is to pause the threads
+        //thread.Resume(); //This is to resume a pause thread
+        //thread.Abort(); //This is to destroy thread
+        thread.Join(); //it is a function which waits until the activity of the thread is all done
+        /*****************************************************************************************************\
+        | When you are creating a new class which works like a thread, you should create a run() function.    |
+        | On this run function, you could try putting up some functions like Thread.sleep( int millisecs),    |
+        | and many more functions.                                                                            |
+        \*****************************************************************************************************/
+    }
+    private static void WorkerOperation()
+    {
+        Console.WriteLine("Simple worker");
+    }
+}
